bc6a7b2405e51c5b72bd1e4a0d9b53df5a98bcbf
majorupdate <tjdals04183@naver.com>
2022-11-03 21:28:06 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 92 ms, Memory: 43152 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
 
-char arr[6561][6561] = {0};
+char arr[6561][6561];
 
 void fractal(int N, int i, int j)
 {
     if (N == 3)
     {
-        arr[i - 1][j - 1] = '*';
-        arr[i - 1][j] = '*';
-        arr[i - 1][j + 1] = '*';
-        arr[i][j - 1] = '*';
-        arr[i][j + 1] = '*';
-        arr[i + 1][j - 1] = '*';
-        arr[i + 1][j] = '*';
-        arr[i + 1][j + 1] = '*';
+        for(int n = i - 1; n <= i + 1; n++)
+        {
+            for(int m = j - 1; m <= j + 1; m++)
+            {
+                if(n != i || m != j)
+                {
+                    arr[n][m] = '*';
+                }
+            }
+        }
     }
 
     else
     {
-        fractal(N / 3, i - (N / 3), j - (N / 3));
-        fractal(N / 3, i - (N / 3), j);
-        fractal(N / 3, i - (N / 3), j + (N / 3));
-
-        fractal(N / 3, i, j - (N / 3));
-        fractal(N / 3, i, j + (N / 3));
-
-        fractal(N / 3, i + (N / 3), j - (N / 3));
-        fractal(N / 3, i + (N / 3), j);
-        fractal(N / 3, i + (N / 3), j + (N / 3));
+        for(int n = i - (N / 3); n <=  i + (N / 3); n += (N / 3))
+        {
+            for(int m = j - (N / 3); m <= j + (N / 3); m += (N / 3))
+            {
+                if(n != i || m != j)
+                {
+                    fractal(N / 3, n, m);
+                }
+            }
+        }
     }
 }
 
 int main()
 {
-
     int N;  scanf("%d", &N);
-
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < N; j++)
-        {
-            arr[i][j] = ' ';
-        }
-    }
 
     fractal(N, N / 2, N / 2);
 
-
     for(int i = 0; i < N; i++)
     {
         for(int j = 0; j < N; j++)
         {
-            printf("%c", arr[i][j]);
+            if(arr[i][j] != '*')
+            {
+                printf("%c", ' ');
+            }
+            else
+            {
+                printf("%c", arr[i][j]);
+            }
         }
         printf("\n");
     }
 
     return 0;
-}   
+}
